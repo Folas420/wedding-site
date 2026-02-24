72aa953d13bea1621dbf14b6380b2cfa36a93037
Folas420 <foncho420420@gmail.com>
2026-02-24 21:31:46 +0100

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,13 @@
+const withNextIntl = require('next-intl/plugin')();
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // This allows production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Also ignore linting errors to be safe
     ignoreDuringBuilds: true,
-  },
+  }
 };
-
-export default nextConfig;
+ 
+module.exports = withNextIntl(nextConfig);
